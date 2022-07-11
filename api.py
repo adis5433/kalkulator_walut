77bb6af461b3bcdb347808009464315f816b24cc
Adrian.Swoboda <adrian.swoboda93@gmail.com>
2022-07-11 21:14:26 +0200

    fix csv converter

--- api.py
+++ api.py
@@ -10,15 +10,21 @@ rates = data[0]["rates"]
 
 headers_of_columns = list(rates[0].keys())
 
-pprint(rates)
+print(rates)
 
-
+print(headers_of_columns)
 codes_of_currencies = []
 for x in range(len(rates)):
     code_of_currency = rates[x]['code']
     codes_of_currencies.append(code_of_currency)
 print(codes_of_currencies)
 
-writer = csv.DictWriter (open('rates.csv', 'w', newline=''),delimiter=';', fieldnames=headers_of_columns)
-writer.writeheader()
-writer.writerows(rates)
+
+with open('rates.csv', 'w', newline='') as csvfile:
+    fieldnames = headers_of_columns
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=";")
+
+    writer.writeheader()
+    for data in rates:
+        writer.writerow(data)
+
